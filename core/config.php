73c1d4ea4bcad3faffd08b23e7bee7574ebbe7c7
leoonsy <yashanovsky.sergey@ya.ru>
2019-12-15 18:00:53 +0500

    add optimization and change db config

--- core/config.php
+++ core/config.php
@@ -3,8 +3,8 @@
 abstract class Config
 {
 	const DB_HOST = "localhost";
-	const DB_USER = "leoon";
-	const DB_PASSWORD = "1762354";
+	const DB_USER = "course_admin";
+	const DB_PASSWORD = "1234567";
 	const DB_NAME = "dbms_course";
 	const SITE_NAME = "http://dbms-course.ru";
 }
